94a6541ffe926371784b1f64ce9d9355b05d7a23
CrackheadAsh <Ashley.rickit@gmail.com>
2020-07-24 13:57:52 +1200

    PlayerMovement
    someshit man

---
+++ Polyball/Assets/Scripts/Script_PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_PlayerController : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    SphereCollider Collider;
+
+    public Vector3 movementForce;
+    void processInput()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rigidBody.AddForce(movementForce);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rigidBody.AddForce(-movementForce);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+         rigidBody = this.GetComponent<Rigidbody>();
+         Collider = this.GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+}
