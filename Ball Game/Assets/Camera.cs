27e39848ff6829b98f6993b1c27bf2cc7c7402fa
Seb <sebastian@thevalentines.net>
2020-07-24 13:51:50 +1200

    First stuff

---
+++ Ball Game/Assets/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    GameObject Player;
+    Vector3 offset;
+
+    void Start()
+    {
+        Player = GameObject.Find("PlayerBall");
+        offset = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float newXPosition = Player.transform.position.x + offset.x;
+        float newZPosition = Player.transform.position.z + offset.z;
+
+        transform.position = new Vector3(newXPosition, transform.position.y, newZPosition);
+    }
+}
