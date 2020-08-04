c195ce3f6edb3b046e2e2172e4c6ddc681aa66e8
Yep <Ashley.rickit@gmail.com>
2020-08-04 13:30:33 +1200

    i dont even know
    yep

---
+++ Polyball/Assets/playerPowerUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerPowerUp : MonoBehaviour
+{
+    BoxCollider compoCollider;
+    public SphereCollider coll_Player;
+    public GameObject obj_Player;
+
+    bool isEquipped = false;
+
+    private void OnCollisionEnter(Collider _other)
+    {
+        Debug.Log("pepega");
+        /*if (_other.gameObject.tag == obj_Player.tag)
+        {
+            Debug.Log("retard");
+            this.transform.SetParent(obj_Player.transform);
+            
+        }*/
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj_Player = GameObject.Find("Player");
+        coll_Player = obj_Player.GetComponent<SphereCollider>();
+        compoCollider = this.GetComponent<BoxCollider>();
+    }
+    
+}
